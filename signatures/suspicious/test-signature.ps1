0921e919b4d483e8ffd5ba53708620d16cc3245b
en20 <enzolozano90@gmail.com>
2025-08-01 14:43:51 -0300

    Add detailed documentation for test signature validation script

--- signatures/suspicious/test-signature.ps1
+++ signatures/suspicious/test-signature.ps1
@@ -1,1 +1,25 @@
+<#
+.SYNOPSIS
+Test signature for validation purposes
+
+.DESCRIPTION
+This is a test signature used for validation testing. It detects suspicious test commands that might be used for testing malicious activities.
+
+.SEVERITY
+medium
+
+.CATEGORY
+other
+
+.SCORE
+50
+
+.REGEX
+Test-Command\s+\-Parameter\s+"[^"]*"
+
+.EXAMPLES
+Test-Command -Parameter "test"
+Test-Command -Parameter "malicious_payload"
+#>
+
 Test-Command -Parameter "test"
